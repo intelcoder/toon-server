35f555df70262ff3ace6e00d22e4e2d85808b757
intelcoder <seongjunkim7162@gmail.com>
2017-03-12 15:31:44 -0400

    oauth updated

--- webtoon/webtoon_server/daum_views/daum_episode_view.py
+++ webtoon/webtoon_server/daum_views/daum_episode_view.py
@@ -2,10 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import WebtoonEpisodes
 from ..serializer import WebtoonEpisodeSerializer
+from rest_framework import permissions
+from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
 from ..webtoon_parser.daum_episode_scraper import DaumEpisodeScraper
 
+
 class DaumEpisodeView(APIView):
+    permission_classes = (permissions.IsAuthenticated, TokenHasReadWriteScope)
     def get(self, request, toon_id=None):
         webtoons = WebtoonEpisodes.objects.all()
         serialized_data = WebtoonEpisodeSerializer(webtoons).data
