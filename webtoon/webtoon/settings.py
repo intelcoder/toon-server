d95094363535c14686ee863ccf0088ee891bde53
Seongjun kim <seongjunkim@Seongjuns-MacBook-Pro.local>
2017-05-03 09:57:30 -0400

    docker setup and postgres added

--- webtoon/webtoon/settings.py
+++ webtoon/webtoon/settings.py
@@ -109,14 +109,13 @@ WSGI_APPLICATION = 'webtoon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'webtoon',
-        # 'USER': 'webtoon',
-        # 'PASSWORD': 'welcome',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '3307',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
