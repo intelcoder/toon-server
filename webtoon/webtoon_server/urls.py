dbbfe93ebaf5dff54e8920e567ea33370437ca56
intelcoder <seongjunkim7162@gmail.com>
2017-07-16 12:38:38 -0400

    Added kakao view to url

--- webtoon/webtoon_server/urls.py
+++ webtoon/webtoon_server/urls.py
@@ -10,6 +10,8 @@ from .daum_views.daum_toon_list_view import DaumToonListView
 from .daum_views.daum_episode_view import DaumEpisodeView
 from .daum_views.daum_init import DaumInit, DaumInitEpisode
 
+from .kakao_views.kakao_init import KakaoInitView, KakaoEpisodeView, KakaoImageView
+
 from .webtoon_views.webtoon_episode_view import WebtoonEpisodeList
 from .webtoon_views.webtoon_view import WebtoonListView, WebtoonDetail, WebtoonFavorite
 from .webtoon_views.webtoon_toon_image_view import WebtoonToonImageView
@@ -19,17 +21,22 @@ urlpatterns = [
     url(r'^$', WebtoonListView.as_view()),
     url(r'^favorite$', WebtoonFavorite.as_view()),
     url(r'^(?P<toon_id>[0-9a-zA-Z]+)/$', WebtoonDetail.as_view()),
-    url(r'^^(?P<toon_id>[0-9a-zA-Z]+)/episode/$', WebtoonEpisodeList.as_view()),
-    url(r'^^(?P<toon_id>[0-9a-zA-Z]+)/episode/(?P<episode_num>[0-9]+)/$', WebtoonEpisodeList.as_view()),
-    url(r'^^(?P<toon_id>[0-9a-zA-Z]+)/episode/(?P<episode_num>[0-9]+)/toon/$', WebtoonToonImageView.as_view()),
+    url(r'^^(?P<toon_id>[0-9a-zA-Z]+)/episode/$',
+        WebtoonEpisodeList.as_view()),
+    url(r'^^(?P<toon_id>[0-9a-zA-Z]+)/episode/(?P<episode_num>[0-9]+)/$',
+        WebtoonEpisodeList.as_view()),
+    url(r'^^(?P<toon_id>[0-9a-zA-Z]+)/episode/(?P<episode_num>[0-9]+)/toon/$',
+        WebtoonToonImageView.as_view()),
 
     url(r'^naver$', NaverToonList.as_view()),
     url(r'^naver/(?P<toon_id>[0-9]+)$', NaverListDetail.as_view()),
     url(r'^naver/init$', NaverInit.as_view()),
     url(r'^naver/init/episode$', NaverInitEpisode.as_view()),
     url(r'^naver/(?P<toon_id>[0-9]+)/episode$', NaverEpisodeList.as_view()),
-    url(r'^naver/(?P<toon_id>[0-9]+)/episode/(?P<episode_num>[0-9]+)$', NaverEpisodeView.as_view()),
-    url(r'^naver/(?P<toon_id>[0-9]+)/episode/(?P<episode_num>[0-9]+)/toon', NaverToonView.as_view()),
+    url(r'^naver/(?P<toon_id>[0-9]+)/episode/(?P<episode_num>[0-9]+)$',
+        NaverEpisodeView.as_view()),
+    url(r'^naver/(?P<toon_id>[0-9]+)/episode/(?P<episode_num>[0-9]+)/toon',
+        NaverToonView.as_view()),
 
     # Daum
     url(r'^daum$', DaumToonListView.as_view()),
@@ -38,5 +45,8 @@ urlpatterns = [
     url(r'^daum/(?P<toon_id>[a-zA-Z]+)/episode$', DaumEpisodeView.as_view()),
     # url(r'^daum/(?P<toon_id>[0-9]+)/episode/(?P<episode_num>[0-9]+)', NaverEpisodeView.as_view()),
     # url(r'^daum/(?P<toon_id>[0-9]+)/episode/(?P<episode_num>[0-9]+)/toon', NaverToonView.as_view()),
+    url(r'^kakao$', KakaoInitView.as_view()),
+    url(r'^kakao/episode$', KakaoEpisodeView.as_view()),
+    url(r'^kakao/images$', KakaoImageView.as_view()),
 
 ]
